cc48040fbfd5fd44c92cbc81340f77bf9b794e70
Olfendren <arthur.bouchaud123@gmail.com>
2021-10-01 12:18:34 +0000

    Question 1.2 Première partie

--- ps_tp1/server_2.c
+++ ps_tp1/server_2.c
@@ -9,77 +9,47 @@
 bool running = 1;
 
 /*Fonction stop_handler*/
-static void stop_handler(int sig)
+void stop_handler(int sig)
 {
-    
+
     char str[sig];
     printf("Signal numéro:  ");
-    printf("%i ",sig);
+    printf("%i ", sig);
     printf("\n");
     printf("La boucle a été arrêtée");
-    
-    running=0;
+
+    running = 0;
 }
-
-
 
 int main()
 {
 
     struct sigaction sa;
+    sa.sa_flags = 0;
+    sa.sa_handler = &stop_handler;
+    sigaction(SIGINT, &sa, NULL);
 
-    struct sigaction new_action, old_action;
-    new_action.sa_handler = stop_handler;
-    sigemptyset (&new_action.sa_mask);
-    new_action.sa_flags = 0;
-
-    /*Le message à afficher (si le booléen est true)*/
-    if(running==1)
+    printf("Boucle infinie\n");
+    /*Boucle Infinie*/
+    while (running)
     {
-        printf("Boucle Infinie, c'est parti\n");
-        /*Boucle Infinie*/
-        for (;;)
-        {
-            
-            /* Vérification de la condition d'execution*/
-            if(running==1)
-            {
-                
-                /*Id des process*/
-                pid_t process_id;
-                pid_t p_process_id;
-                process_id = getpid();
-                p_process_id = getppid();
-                printf("L'ID du process: %d\n",process_id);
-                printf("L'ID du process père: %d\n",p_process_id);
+        /*Id des process*/
+        pid_t process_id;
+        pid_t p_process_id;
+        process_id = getpid();
+        p_process_id = getppid();
+        printf("L'ID du process: %d\n", process_id);
+        printf("L'ID du process père: %d\n", p_process_id);
 
-                /*Nb aléatoire entre 0 et 99*/
-                printf("Nombre aléatoire entre 0 et 99: %d\n", rand() % 99);
+        /*Nb aléatoire entre 0 et 99*/
+        printf("Nombre aléatoire entre 0 et 99: %d\n", rand() % 99);
 
-                /*Sleep*/
-                printf("Endormissement pour 1 seconde\n");
-                sleep(1);
-                printf("\n"); 
-
-               int returnvalue = sigaction(SIGINT, &sa,NULL);
-                if (returnvalue==-1)
-                {
-                    printf("kek");
-                }
-                
-
-            }
-            else
-            {
-                printf("\n");
-                return 0;
-            }
-            
-        }          
+        /*Sleep*/
+        printf("Endormissement pour 1 seconde\n");
+        sleep(1);
+        printf("\n");
     }
-    
-    
+
     printf("Fin de la boucle \n");
     return EXIT_SUCCESS;
-     
 }
