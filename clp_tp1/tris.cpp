490066c29654e2d28997dcb7c009f19dba1c5124
Olfendren <arthur.bouchaud123@gmail.com>
2021-10-05 12:09:06 +0000

    Deuxième Commit Question 1.2

--- clp_tp1/tris.cpp
+++ clp_tp1/tris.cpp
@@ -4,7 +4,7 @@
 
 
 
- double print_tab(std::vector<int> v)
+ double print_tab(std::vector<double> v)
 {
     std::cout<<"v=[";
     for (int n :v)
@@ -15,16 +15,34 @@
     return 0;
 }
 
+std::vector<double> random_tab(std::vector<double> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {         
+    v[i] = rand()%21 + (-10);         
+    }
+    return v;
+}
+
 double test_11()
 {
     std::cout<<"***Test 11*** \n";
-    const std::vector< int > tab{ 1, -2, 3, -4, 5, -6 };
+    const std::vector< double > tab{ 1, -2, 3, -4, 5, -6 };
     print_tab(tab);
 
     return 0;
 }
 
+double test_12()
+{
+    std::vector< double > tab( 10 );
+    std::vector< double > tabr= random_tab(tab);
+    print_tab(tabr);
+    return 0;
+}
+
 int main()
 {
-    test_11();
+   std::srand(time(0));
+   test_12();
 }
