f5aba57911efbe5cafd71681e48a617d0863894e
Olfendren <arthur.bouchaud123@gmail.com>
2021-10-05 15:25:00 +0000

    Deuxième Question Deuxième Partie 2.2

--- clp_tp1/listes.cpp
+++ clp_tp1/listes.cpp
@@ -27,16 +27,38 @@ void print_list(const std::forward_list<int> &list)
     std::cout<<") \n";
 }
 
-void test_11()
+std::forward_list< int > map_iter(const std::forward_list<int> &list, std::function<int( int )>sig)
+{
+    std::forward_list<int>res;
+
+    for (auto const& i : list) 
+    {
+        res.push_front(sig(i));
+
+    }  
+return res;
+}
+
+
+
+
+void test_21()
 {
     std::cout<<"***Test 2.1*** \n";
     std::forward_list< int > list{ random_list(10) };
     print_list(list);
 }
+void test_22()
+{
+    std::forward_list< int > list{ random_list(10) };
+    print_list(list);
+    auto res=map_iter(list,[](int a) {return a*3; });
+    print_list(res);
 
+}
 
 int main ()
 {
     std::srand(time(0));
-    test_11();
+    test_22();
 }
